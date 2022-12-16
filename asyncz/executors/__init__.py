a2934992397c5bdae1b34349b8ed0552ea9a3613
Tiago A. Silva <tiago.arasilva@outlook.com>
2022-12-16 12:27:08 +0000

    🪛 Added esmerald contrib.
    * Handler
    * Decorator
    * Types

--- asyncz/executors/__init__.py
+++ asyncz/executors/__init__.py
@@ -1,5 +1,12 @@
 from .asyncio import AsyncIOExecutor
 from .base import BaseExecutor
 from .debug import DebugExecutor
+from .pool import ProcessPoolExecutor, ThreadPoolExecutor
 
-__all__ = ["BaseExecutor", "AsyncIOExecutor", "DebugExecutor"]
+__all__ = [
+    "BaseExecutor",
+    "AsyncIOExecutor",
+    "DebugExecutor",
+    "ProcessPoolExecutor",
+    "ThreadPoolExecutor",
+]
