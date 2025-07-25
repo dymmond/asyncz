354249b786f185a97d8fd42c5c38560dca550284
alex <devkral@web.de>
2025-07-25 19:18:35 +0200

    update and align ruff, add example for tz conversion error

---
+++ tests/test_real_examples.py
@@ -0,0 +1,42 @@
+import asyncio
+from datetime import UTC, datetime
+from zoneinfo import ZoneInfo
+
+import pytest
+from freezegun import freeze_time
+
+from asyncz.schedulers import NativeAsyncIOScheduler
+from asyncz.triggers import CronTrigger
+
+
+@pytest.mark.parametrize("tz", ["Etc/UTC", "Asia/Kolkata"])
+@pytest.mark.asyncio(loop_scope="function")
+async def test_tz_cron(tz):
+    time_zone_string = "Asia/Kolkata"
+    trigger = CronTrigger.from_crontab("0 8 * * *", time_zone_string)
+    scheduler = NativeAsyncIOScheduler(timezone=tz)
+
+    called: int = 0
+
+    def execution_function():
+        nonlocal called
+        called += 1
+
+    scheduler.add_task(
+        execution_function,
+        trigger=trigger,
+        id="some_random_id",
+    )
+    with freeze_time(
+        datetime(2025, 5, 1, 7, 59, 59, tzinfo=ZoneInfo(time_zone_string)), tick=True
+    ) as t:
+        async with scheduler:
+            assert called == 0
+            await asyncio.sleep(2)
+            assert called == 1
+            t.move_to(
+                datetime(2025, 5, 2, 7, 59, 59, tzinfo=ZoneInfo(time_zone_string)).astimezone(UTC)
+            )
+            assert called == 1
+            await asyncio.sleep(2)
+            assert called == 2
